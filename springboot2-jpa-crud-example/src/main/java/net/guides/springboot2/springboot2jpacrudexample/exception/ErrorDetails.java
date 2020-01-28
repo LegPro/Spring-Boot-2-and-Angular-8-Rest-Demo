e620bf47ae8ae0df962a9644aa60bcef2ef4f35f
LegPro <vhinukale@gmail.com>
2020-01-28 14:14:08 +0530

    Demo

---
+++ springboot2-jpa-crud-example/src/main/java/net/guides/springboot2/springboot2jpacrudexample/exception/ErrorDetails.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package net.guides.springboot2.springboot2jpacrudexample.exception;
+import java.util.Date;
+
+/**
+ * @author vhinu
+ *
+ */
+public class ErrorDetails {
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    public ErrorDetails(Date timestamp, String message, String details) {
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+         return message;
+    }
+
+    public String getDetails() {
+         return details;
+    }
+}
---
+++ springboot2-jpa-crud-example/src/main/java/net/guides/springboot2/springboot2jpacrudexample/model/package-info.java
@@ -0,0 +1,1 @@
+package net.guides.springboot2.springboot2jpacrudexample.model;
